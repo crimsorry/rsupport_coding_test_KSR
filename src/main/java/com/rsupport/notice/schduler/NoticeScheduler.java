f6049208c7b9783be4d911339b7b89244045e470
creamsorry <sooor.ee1@gmail.com>
2025-02-11 15:08:28 +0900

    [feat] 스케줄링 추가

--- src/main/java/com/rsupport/notice/schduler/NoticeScheduler.java
+++ src/main/java/com/rsupport/notice/schduler/NoticeScheduler.java
@@ -1,13 +1,25 @@
 package com.rsupport.notice.schduler;
 
+import com.rsupport.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class NoticeScheduler {
 
+    private final NoticeService noticeService;
+
+    @Transactional
+    @Scheduled(fixedRate = 60000)
+    public void disableExpiredQuote() {
+        log.info("# [NoticeScheduler] ::: Scheduler Start");
+        noticeService.syncViewCountToDB();
+        log.info("# [NoticeScheduler] ::: Scheduler End");
+    }
 
 }
